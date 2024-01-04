bddca9eb5705d643ba326e6201a552fd1b243d4f
mrinalsinha2211 <145749474+mrinalsinha2211@users.noreply.github.com>
2024-01-04 10:37:19 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ const fileName = "myfile.txt";
 const updateFile = async (fileName, fileContent) => {
   // write code here
   // dont change function name
-  
+   try{
+    const updated=await fs.appendFile(fileName,fileContent,"Newton School, An online learning platform");
+    console.log(updated);
+    return updated;
+  }catch(error){
+      console.log(`error in update a file ${error.message}`);
+      return null;
+  }
 };
 
 module.exports = updateFile;
